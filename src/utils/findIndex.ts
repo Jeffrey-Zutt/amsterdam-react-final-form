5fb914db65b6f516738da38ddc47fde27db4a035
Jeffrey Zutt <jeffrey.zutt@leaseplan.com>
2020-04-30 15:51:53 +0200

    fix: CheckboxFields crashed when no initialdata was set

--- src/utils/findIndex.ts
+++ src/utils/findIndex.ts
@@ -3,7 +3,11 @@ import _isEqual from "lodash/isEqual"
 export const findIndex = <T>(haystack:T[], needle:T) => haystack
   .findIndex((item) => _isEqual(item, needle))
 
-export const findIndexes = <T>(haystack:T[], needles:T[]) => {
+export const findIndexes = <T>(haystack:T[]|undefined, needles:T[]|undefined) => {
+  if (!haystack || !needles) {
+    return []
+  }
+
   const indexes:number[] = []
   haystack.forEach((item, index) => {
     needles.forEach(needle => {
