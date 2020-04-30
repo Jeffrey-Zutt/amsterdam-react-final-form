34f650d69ab4e17fbc4bd033aca5dde71195f36c
Jeffrey Zutt <jeffrey.zutt@leaseplan.com>
2020-04-30 15:54:47 +0200

    chore: ran storybook:build

---
+++ docs/precache-manifest.199dc88b24062438b56700e341b01f6e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19b4443722ac9939e9e87e86b5735a5a",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.88780b9efd317b368435.bundle.js"
+  },
+  {
+    "url": "runtime~main.88780b9efd317b368435.bundle.js"
+  },
+  {
+    "url": "vendors~main.88780b9efd317b368435.bundle.js"
+  },
+  {
+    "url": "vendors~main.88780b9efd317b368435.bundle.js.LICENSE.txt"
+  }
+]);
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.694c0f467b22530e8880d64a68fce9fe.js"
+  "precache-manifest.199dc88b24062438b56700e341b01f6e.js"
 );
 
 self.addEventListener('message', (event) => {
