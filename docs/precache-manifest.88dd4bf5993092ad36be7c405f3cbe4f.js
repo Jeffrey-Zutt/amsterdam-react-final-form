2752eb7edd75fe21e42fd5b59ecf539824bb6a5e
Jeffrey Zutt <jeffrey.zutt@leaseplan.com>
2020-05-27 12:27:18 +0200

    chore: ran storybook:build

---
+++ docs/precache-manifest.88dd4bf5993092ad36be7c405f3cbe4f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce3fd3e45852a6bf436b651b410791d4",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.c90addf93c823db099dc.bundle.js"
+  },
+  {
+    "url": "runtime~main.c90addf93c823db099dc.bundle.js"
+  },
+  {
+    "url": "vendors~main.c90addf93c823db099dc.bundle.js"
+  },
+  {
+    "url": "vendors~main.c90addf93c823db099dc.bundle.js.LICENSE.txt"
+  }
+]);
