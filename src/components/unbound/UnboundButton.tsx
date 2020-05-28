75c927cba415ee6d9f80863f127ada584a4db3e6
Jeffrey Zutt <jeffrey.zutt@leaseplan.com>
2020-05-28 16:23:29 +0200

    feat: introduced horizontalyAligned buttons

--- src/components/unbound/UnboundButton.tsx
+++ src/components/unbound/UnboundButton.tsx
@@ -1,11 +1,26 @@
 import React from "react"
-import { Button } from "@datapunt/asc-ui"
+import { Button, themeSpacing } from "@datapunt/asc-ui"
+import styled, { css } from "styled-components"
 import ComposedField, { ComposedFieldProps } from "./ComposedField"
+import { Responsive, responsiveProps } from "../layout/responsiveProps"
 
-export type Props = React.ComponentProps<typeof Button> & ComposedFieldProps
+export type Props = React.ComponentProps<typeof Button> & ComposedFieldProps & {
+  alignedHorizontally?: Responsive<boolean>
+}
+
+type StyledButtonProps = Pick<Props, "alignedHorizontally">
+const StyledButton = styled(Button)<StyledButtonProps>`
+  margin-top: ${ themeSpacing(1) };
+  
+  ${ ((props:StyledButtonProps) => responsiveProps(props, {
+    // Its aesthetically more pleasing if horizontally aligned buttons have a negative top-margin.
+    // You have to set the property yourself though
+    alignedHorizontally: unit => css`margin-top: ${ themeSpacing( unit ? -1 : 1) };`
+  })) }
+`
 
 const UnboundButton:React.FC<Props> = ({ position, align, ...restProps }) => (<ComposedField position={position} align={align}>
-  <Button {...restProps} />
+  <StyledButton {...restProps} />
 </ComposedField>)
 
 export default UnboundButton
