84eb22d212971d85fbf71c15895c742a694fc1d6
Jeffrey Zutt <jeffrey.zutt@leaseplan.com>
2020-06-29 15:26:05 +0200

    feat: ScaffoldForm has a property 'showSpinner'

--- src/components/final-form/Scaffold/ScaffoldForm.tsx
+++ src/components/final-form/Scaffold/ScaffoldForm.tsx
@@ -1,8 +1,11 @@
 import React, { PropsWithChildren, useCallback } from "react"
+import { themeColor, Spinner } from "@datapunt/asc-ui"
+import styled from "styled-components"
 import { Form, FormProps } from "react-final-form"
 import { FormApi, SubmissionErrors } from "final-form"
 
 type Props<FormValues> = Omit<FormProps<FormValues>, "onSubmit"> & {
+  showSpinner?: boolean
   successComponent?: JSX.Element
   errorComponent?: JSX.Element
   hasError?: boolean
@@ -11,7 +14,29 @@ type Props<FormValues> = Omit<FormProps<FormValues>, "onSubmit"> & {
   onReset?: () => void
 }
 
-function ScaffoldForm<T>({ onReset, onSubmit, onOriginalSubmit, children, successComponent, errorComponent, hasError, ...restProps }:PropsWithChildren<Props<T>>) {
+const Wrap = styled.div`
+  position: relative;
+`
+
+const Overlay = styled.div`
+  position: absolute;
+  left: 0;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  
+  background-color: ${ themeColor("tint", "level0") };
+  opacity: .5;
+`
+
+const CenteredSpinner = styled(Spinner)`
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+`
+
+function ScaffoldForm<T>({ onReset, onSubmit, onOriginalSubmit, children, successComponent, errorComponent, hasError, showSpinner, ...restProps }:PropsWithChildren<Props<T>>) {
   const handleSubmit = useCallback(
     (values:T, form: FormApi<T>, callback?: (errors?: SubmissionErrors) => void) => {
       if (onOriginalSubmit !== undefined) {
@@ -28,14 +53,17 @@ function ScaffoldForm<T>({ onReset, onSubmit, onOriginalSubmit, children, succes
   return (<Form<T>
       {...restProps}
       onSubmit={handleSubmit}
-      render={({ handleSubmit, submitSucceeded, submitFailed }) => (
-        <form onSubmit={handleSubmit} onReset={onReset}>
-          { hasError && errorComponent }
-          { !hasError && submitSucceeded && successComponent
-              ? successComponent
-              : children
-          }
-        </form>
+      render={({ handleSubmit, submitSucceeded }) => (
+        <Wrap>
+          { showSpinner && (<Overlay><CenteredSpinner /></Overlay>) }
+          <form onSubmit={handleSubmit} onReset={onReset}>
+            { hasError && errorComponent }
+            { !hasError && submitSucceeded && successComponent
+                ? successComponent
+                : children
+            }
+          </form>
+        </Wrap>
       )}
     />
   )
