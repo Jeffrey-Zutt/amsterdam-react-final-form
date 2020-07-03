9261eff0352695242c61c52ee3687a1297a53ac1
Jeffrey Zutt <jeffrey.zutt@leaseplan.com>
2020-07-03 12:11:14 +0200

    chore: ran storybook:build

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.6aea0d2c5d952d4e583d85f1b8262fed.js"
+  "precache-manifest.cfb797e7cb08877ede92ad223a6e8d2c.js"
 );
 
 self.addEventListener('message', (event) => {
