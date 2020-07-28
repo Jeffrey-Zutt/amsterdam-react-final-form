a577b9bf728c0c3506ac6d9c4208da303c2b78da
Jeffrey Zutt <jeffrey.zutt@leaseplan.com>
2020-07-28 15:04:46 +0200

    feat: disable submitbutton when form is invalid

--- src/components/final-form/SubmitButton/SubmitButton.tsx
+++ src/components/final-form/SubmitButton/SubmitButton.tsx
@@ -5,13 +5,13 @@ import { Spinner } from "@datapunt/asc-ui"
 import UnboundButton, { Props as UnboundButtonProps } from "../../unbound/UnboundButton"
 
 const SubmitButton:React.FC<UnboundButtonProps> = ({ onClick, label, ...otherProps }) => {
-  const { submitting, dirty, hasValidationErrors } = useFormState()
+  const { submitting, invalid } = useFormState()
 
   return <UnboundButton
     type="submit"
     icon={ submitting ? <Spinner /> : undefined }
     variant="secondary"
-    disabled={ submitting || hasValidationErrors || !dirty }
+    disabled={ submitting || invalid }
     data-e2e-id="submit"
     { ...otherProps }
   >
