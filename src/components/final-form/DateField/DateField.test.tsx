7baf19cbd7d77fe82ac80c7f660e2a1116efd808
Remco Hoff <remco@code010.nl>
2021-02-18 11:33:03 +0100

    Update DateField.test.tsx

--- src/components/final-form/DateField/DateField.test.tsx
+++ src/components/final-form/DateField/DateField.test.tsx
@@ -2,14 +2,14 @@ import React from "react"
 import { mount } from "enzyme"
 import DateField from "./DateField"
 import { wrapInForm } from "../__test__/wrapInForm"
-// import { FieldError } from "../../unbound/FieldError"
+import { FieldError } from "../../unbound/FieldError"
 
 describe("DateField", () => {
   const onSubmit = jest.fn()
 
   const component = mount(wrapInForm(
     onSubmit,
-    { myField: "myValue" },
+    { myField: "31/01/2021" },
     <DateField name='myField' />
   ))
 
@@ -18,75 +18,75 @@ describe("DateField", () => {
   })
 
   it("should set an initial value", () => {
-    expect(component.find("input").prop("value")).toEqual("myValue")
+    expect(component.find("input").prop("value")).toEqual("31/01/2021")
   })
 
-  // it("should propagate its changes to the wrapping form", () => {
-  //   component
-  //     .find("input")
-  //     .simulate("change", { target: { value: "Changed value" } })
+  it("should propagate its changes to the wrapping form", () => {
+    component
+      .find("input")
+      .simulate("change", { target: { value: "31/01/2022" } })
 
-  //   component
-  //     .find("form")
-  //     .simulate("submit")
+    component
+      .find("form")
+      .simulate("submit")
 
-  //   expect(onSubmit)
-  //     .toHaveBeenCalledWith({
-  //       "myField": "Changed value" },
-  //       expect.anything(),
-  //       expect.anything()
-  //     )
-  // })
+    expect(onSubmit)
+      .toHaveBeenCalledWith({
+        "myField": "31/01/2022" },
+        expect.anything(),
+        expect.anything()
+      )
+  })
 
-  // describe("when a validation error is set", () => {
-  //   const component = mount(wrapInForm(
-  //     onSubmit,
-  //     { myField: "myValue" },
-  //     <DateField name='myField' validate={() => "always errors"} />
-  //   ))
+  describe("when a validation error is set", () => {
+    const component = mount(wrapInForm(
+      onSubmit,
+      { myField: "31/01/2021" },
+      <DateField name='myField' validate={() => "always errors"} />
+    ))
 
-  //   it("should NOT show a FieldError", () => {
-  //     expect(component.find(FieldError).exists()).toEqual(false)
-  //   })
+    it("should NOT show a FieldError", () => {
+      expect(component.find(FieldError).exists()).toEqual(false)
+    })
 
-  //   describe("when a user interacts with the component", () => {
-  //     beforeEach(() => {
-  //       component
-  //         .find("input")
-  //         .simulate("focus")
-  //         .simulate("change", { target: { value: "Changed value" } })
-  //         .simulate("blur")
-  //     })
+    describe("when a user interacts with the component", () => {
+      beforeEach(() => {
+        component
+          .find("input")
+          .simulate("focus")
+          .simulate("change", { target: { value: "31/01/2021" } })
+          .simulate("blur")
+      })
 
-  //     it("should show a FieldError", () => {
-  //       expect(component.find(FieldError).text()).toEqual("always errors")
-  //     })
-  //   })
-  // })
+      it("should show a FieldError", () => {
+        expect(component.find(FieldError).text()).toEqual("always errors")
+      })
+    })
+  })
 
-  // describe("when isRequired is set", () => {
-  //   const component = mount(wrapInForm(
-  //     onSubmit,
-  //     { myField: "myValue" },
-  //     <DateField name='myField' isRequired={true} />
-  //   ))
+  describe("when isRequired is set", () => {
+    const component = mount(wrapInForm(
+      onSubmit,
+      { myField: "31/01/2021" },
+      <DateField name='myField' isRequired={true} />
+    ))
 
-  //   it("should NOT show a FieldError", () => {
-  //     expect(component.find(FieldError).exists()).toEqual(false)
-  //   })
+    it("should NOT show a FieldError", () => {
+      expect(component.find(FieldError).exists()).toEqual(false)
+    })
 
-  //   describe("when a user interacts with the component", () => {
-  //     beforeEach(() => {
-  //       component
-  //         .find("input")
-  //         .simulate("focus")
-  //         .simulate("change", { target: { value: "" } })
-  //         .simulate("blur")
-  //     })
+    describe("when a user interacts with the component", () => {
+      beforeEach(() => {
+        component
+          .find("input")
+          .simulate("focus")
+          .simulate("change", { target: { value: "" } })
+          .simulate("blur")
+      })
 
-  //     it("should show a FieldError", () => {
-  //       expect(component.find(FieldError).text()).toEqual("Dit veld is verplicht")
-  //     })
-  //   })
-  // })
+      it("should show a FieldError", () => {
+        expect(component.find(FieldError).text()).toEqual("Dit veld is verplicht")
+      })
+    })
+  })
 })
