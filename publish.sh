bb41ffa414c3b96c295616794f5ea460f9a7fbdf
Gilles Wittenberg <mail@gilleswittenberg.com>
2021-06-01 14:02:23 +0200

    Improve publish script

--- publish.sh
+++ publish.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
+# increment version and build
 npm version patch
+npm run build
 
+# build storybook and push to repo
 npm run storybook:build
 git add --all
 git commit -m "chore: ran storybook:build"
-
-npm run build
 git push
 
+# publish to npm
 cd ./dist || exit
 npm publish
